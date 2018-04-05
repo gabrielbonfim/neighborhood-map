2cdfe0e341b6c047b6c3f71e07479e7c59721c6f
Gabriel Almeida <gabrielbonfim@gmail.com>
2018-04-05 19:30:37 +0100

    First Commit

--- js/app-k.js
+++ js/app-k.js
@@ -1,14 +1,14 @@
 var allLocations = [
-    {id: 0, name: 'Ponte 25 de Abril'}
-//    {id: 1, name: 'Basílica da Estrela'},
-//    {id: 2, name: 'Torre de Belém'},
-//    {id: 3, name: 'Casa Fernando Pessoa'},
-//    {id: 4, name: 'Palácio Nacional da Ajuda'},
-//    {id: 5, name: 'Planetário Calouste Goulbekian'},
-//    {id: 6, name: 'Centro Cultural de Belém'},
-//    {id: 7, name: 'Museu Nacional dos Coches'},
-//    {id: 8, name: 'Cordoaria Nacional'},
-//    {id: 9, name: 'LxFactory'}
+    {id: 0, name: 'Ponte 25 de Abril'},
+    {id: 1, name: 'Basílica da Estrela'},
+    {id: 2, name: 'Torre de Belém'},
+    {id: 3, name: 'Casa Fernando Pessoa'},
+    {id: 4, name: 'Palácio Nacional da Ajuda'},
+    {id: 5, name: 'Planetário Calouste Goulbekian'},
+    {id: 6, name: 'Centro Cultural de Belém'},
+    {id: 7, name: 'Museu Nacional dos Coches'},
+    {id: 8, name: 'Cordoaria Nacional'},
+    {id: 9, name: 'LxFactory'}
 ];
 
 ko.utils.stringContains = function (string, filter) {        	
